8c33d7753a15930c54d9134f95635a4a73346c24
nlemast <nlemaster47@gmail.com>
2019-07-27 19:52:00 -0400

    built out App component and Tile with fetch request

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import Tile from './Tile';
 
 export class App extends Component {
   render() {
-    return <div>heyooo</div>;
+    return <Tile />;
   }
 }
 
